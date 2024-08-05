8a14c7e6523cf6bc22c9772460b7c2abdb848b40
Goose <112972833+CandyGoose@users.noreply.github.com>
2024-08-05 18:47:06 +0300

    Add Yandex translation integration

--- src/test/java/com/candygoose/translationservice/TranslationServiceApplicationTests.java
+++ src/test/java/com/candygoose/translationservice/TranslationServiceApplicationTests.java
@@ -1,5 +1,7 @@
 package com.candygoose.translationservice;
 
+import com.candygoose.translationservice.model.TranslationRequest;
+import com.candygoose.translationservice.model.TranslationResponse;
 import com.candygoose.translationservice.service.TranslationService;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -16,5 +18,14 @@ class TranslationServiceApplicationTests {
     @Test
     void contextLoads() {
         assertNotNull(translationService);
+    }
+
+    @Test
+    void testBasicTranslation() {
+        TranslationRequest request = new TranslationRequest("en", "ru", "World");
+        TranslationResponse response = translationService.translate(request);
+
+        assertNotNull(response, "Ответ не должен быть null");
+        assertEquals("Мир", response.getResponseText(), "Перевод должен соответствовать ожидаемому результату");
     }
 }
